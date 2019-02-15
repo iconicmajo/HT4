d4736c16c41063bf4768c95d0aa811d79676c92a
iconicmajo <42973036+iconicmajo@users.noreply.github.com>
2019-02-15 12:11:32 -0600

    Add files via upload

---
+++ FactoryImplements.java
@@ -0,0 +1,15 @@
+/**
+ * Factory Implements  
+ * HT4
+ * Javier Salazar 18764
+ * Rodolfo Alonso 17470
+ * Majo Castro 181202
+ */
+
+/**
+ * @author Majo!
+ *
+ */
+public class FactoryImplements {
+
+}
---
+++ FactoryStack.java
@@ -0,0 +1,15 @@
+/**
+ * FactoryStack
+ * HT4 
+ * Javier Salazar 18764
+ * Rodolfo Alonso 17470
+ * Majo Castro 181202
+ */
+
+/**
+ * @author Majo!
+ *
+ */
+public class FactoryStack {
+
+}
