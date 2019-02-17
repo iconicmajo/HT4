5175b88e0d15faa89be701224bc86b21483faa71
iconicmajo <42973036+iconicmajo@users.noreply.github.com>
2019-02-16 21:23:42 -0600

    Update DoublyLinkedNode.java

--- DataEstructure_Lab_No.4/src/DoublyLinkedNode.java
+++ DataEstructure_Lab_No.4/src/DoublyLinkedNode.java
@@ -9,6 +9,11 @@
  * @author Majo!
  *
  */
+/**
+ * @author Majo!
+ *
+ * @param <E>
+ */
 public class DoublyLinkedNode<E> {
 	
 	protected E data;
@@ -16,7 +21,13 @@ public class DoublyLinkedNode<E> {
 	protected DoublyLinkedNode<E> previousElement;
 	
 	
-	public DoublyLinkedNode(E v, DoublyLinkedNode<E> next, DoublyLinkedNode<E> previous){
+	/**
+	 * @param v
+	 * @param next
+	 * @param previous
+	 */
+	public DoublyLinkedNode(E v, 
+				DoublyLinkedNode<E> next, DoublyLinkedNode<E> previous){
 		data = v;
 		nextElement = next;
 		
@@ -34,6 +45,32 @@ public class DoublyLinkedNode<E> {
 		// post: constructs a single element
 		{
 		this(v,null,null);
+		}
+
+		public E value() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public DoublyLinkedNode<E> previous() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		public void  setNext(DoublyLinkedNode<E> next) {
+			// TODO Auto-generated method stub
+			nextElement = next;
+		}
+
+		public DoublyLinkedNode<E> next() {
+			// TODO Auto-generated method stub
+			return nextElement;
+		}
+
+		public void setPrevious(DoublyLinkedNode<E> previous) {
+			// TODO Auto-generated method stub
+			previousElement = previous;
+			
 		}
 	}
 
